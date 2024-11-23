d2ff4fe0efa2d69b9507fed3674d323822e421d1
Dmitry Rodionov <dr@idrp.ru>
2024-11-23 08:58:57 +0300

    #521322 создание структуры проекта

---
+++ src/main/java/ru/misis/atyrau/AtyrauApplication.java
@@ -0,0 +1,13 @@
+package ru.misis.atyrau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtyrauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtyrauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/misis/atyrau/AtyrauApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.misis.atyrau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtyrauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
